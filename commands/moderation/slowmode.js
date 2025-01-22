8c464444f1c68732288acb8cde23a5ce4cbfbb39
minhson05102010 <cuson0510@gmail.com>
2025-01-22 22:20:56 +0700

    Update slowmode.js

--- commands/moderation/slowmode.js
+++ commands/moderation/slowmode.js
@@ -3,7 +3,7 @@ const { SlashCommandBuilder } = require('discord.js');
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('slowmode')
-    .setDescription('Sets the slowmode for the channel.')
+    .setDescription('Đặt chế độ chậm cho kênh❄️.')
     .addIntegerOption(option => option.setName('duration').setDescription('Duration in seconds').setRequired(true)),
   async execute(interaction) {
     const duration = interaction.options.getInteger('duration');
